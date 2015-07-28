698d4d419f7f698da3677c8a6a376960391f39a9
Jace Ju <jaceju@gmail.com>
2015-07-28 21:36:12 +0800

    Fix psr-2

--- 1/client.php
+++ 1/client.php
@@ -1,13 +1,14 @@
 <?php
+
 $client = new GearmanClient();
 $client->addServer(); // 預設為 localhost
 $emailData = array(
-    'name'  => 'web',
-      'email' => 'member@example.com',
-    );
+    'name' => 'web',
+    'email' => 'member@example.com',
+);
 $imageData = array(
     'image' => '/var/www/pub/image/test.png',
-  );
+);
 $client->doBackground('sendEmail', serialize($emailData));
 echo "Email sending is done.\n";
 $client->doBackground('resizeImage', serialize($imageData));
--- 1/worker.php
+++ 1/worker.php
@@ -1,10 +1,11 @@
 <?php
+
 $id = microtime(true);
 $worker = new GearmanWorker();
 $worker->addServer(); // 預設為 localhost
 $worker->addFunction('sendEmail', 'doSendEmail');
 $worker->addFunction('resizeImage', 'doResizeImage');
-while($worker->work()) {
+while ($worker->work()) {
     if ($worker->returnCode() != GEARMAN_SUCCESS) {
         break;
     }
@@ -18,6 +19,7 @@ function doSendEmail($job)
     sleep(10); // 模擬處理時間
     echo "$id: Email sending is done really.\n\n";
 }
+
 function doResizeImage($job)
 {
     global $id;
