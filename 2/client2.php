c5d35e35037fbe0133b46f8ae6dda7bed8a0c808
Jace Ju <jaceju@gmail.com>
2015-07-28 21:37:26 +0800

    Fix psr-2

--- 2/client2.php
+++ 2/client2.php
@@ -1,4 +1,5 @@
 <?php
+
 $client = new GearmanClient();
 $client->addServer(); // 預設為 localhost
-$result = $client->doBackground("job-server-01-default_job", "php worker2.php 123 456 789");
+$result = $client->doBackground('job-server-01-default_job', 'php worker2.php 123 456 789');
